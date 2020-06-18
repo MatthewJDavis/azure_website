00a32672beb969f719f705f1590b3df9c6a9552d
MatthewJDavis <matthewdavis111@gmail.com>
2020-06-17 20:27:39 -0400

    added standard files

---
+++ manage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+from dotenv import load_dotenv
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azure_website.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+project_folder = os.path.expanduser(
+    '~/Documents/python/azure_images/azure_website')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
+
+if __name__ == '__main__':
+    main()
+
